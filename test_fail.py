78bff237dd3eea8ec3bb493f5a6fbd5c36856af3
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-02-27 13:00:23 +0500

    2 commit

--- test_fail.py
+++ test_fail.py
@@ -4,3 +4,23 @@ def test_fail():
 
 def test_fail2():
     assert False
+
+
+def test_fail3():
+    assert False
+
+
+def test_fail4():
+    assert False
+
+
+def test_fail5():
+    assert False
+
+
+def test_fail6():
+    assert False
+
+
+def test_fail7():
+    assert False
